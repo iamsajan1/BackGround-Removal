e5b6cb0c0931e3a7efb7bffa6afd5eb187a5ad68
iamsajan1 <kumarsantosh.iict06a@gmail.com>
2026-01-09 18:18:05 +0530

    build issue fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
-import BgSlider from '../components/bgSlider'
+import BgSlider from '../components/BgSlider'
 import Testimonial from '../components/Testimonial'
 import Upload from '../components/Upload'
 
